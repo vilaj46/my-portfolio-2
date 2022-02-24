38b8a006d1973662dfd33f648a56b0d828402981
Julian <vilaj46@gmail.com>
2022-02-24 17:51:20 -0500

    Updated resume page again.

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,7 +7,9 @@ import Container from "../components/pages/Container";
 export default function resume() {
   return (
     <Container>
-      <embed src={pdf} type="application/pdf" width="100%" height="600px" />
+      <div style={{ border: "2px solid red" }}>
+        <iframe src={pdf} title="Resume" width="100%" height="800px"></iframe>
+      </div>
     </Container>
   );
 }
