328016768a2922e8c736c2d93ede4f89ddd22e90
Julian <vilaj46@gmail.com>
2022-02-24 17:54:29 -0500

    Removed border from iframe

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,7 +7,7 @@ import Container from "../components/pages/Container";
 export default function resume() {
   return (
     <Container>
-      <div style={{ border: "2px solid red" }}>
+      <div>
         <iframe src={pdf} title="Resume" width="100%" height="800px"></iframe>
       </div>
     </Container>
